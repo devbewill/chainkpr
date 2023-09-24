730854759bdae173fd422ad0056536a2d2987e4f
devbewill <sedprc@gmail.com>
2023-09-24 22:42:48 +0200

    fixing

--- src/components/Industries.js
+++ src/components/Industries.js
@@ -48,7 +48,7 @@ const industries = [
 export default function Context() {
   return (
     <section className="industries">
-      <div className="overflow-hidden bg-white py-10 sm:py-32">
+      <div className="overflow-hidden bg-white py-20">
         <div className="mx-auto max-w-7xl lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
             <div className="px-6 lg:pr-8 lg:pt-4">
