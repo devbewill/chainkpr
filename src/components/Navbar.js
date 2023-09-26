8baf959a343195b99c837067ec3fe31a53dbae5e
devbewill <sedprc@gmail.com>
2023-09-26 16:47:48 +0200

    fixing navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,20 @@
-import { useState } from "react";
-import { Dialog } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import SwitchTheme from "./SwitchTheme";
+import { Fragment, useState } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
+
+import {
+  ArrowPathIcon,
+  Bars3Icon,
+  ChartPieIcon,
+  CursorArrowRaysIcon,
+  FingerPrintIcon,
+  SquaresPlusIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import {
+  ChevronDownIcon,
+  PhoneIcon,
+  PlayCircleIcon,
+} from "@heroicons/react/20/solid";
 
 const navigation = [
   { name: "About", href: "https://www.mangrovia.solutions/" },
@@ -10,13 +23,46 @@ const navigation = [
   { name: "Docs", href: "https://doc.datome.io/" },
 ];
 
+const usecases = [
+  {
+    name: "Textile",
+    description: "lorem ipsum",
+    href: "#",
+    icon: ChartPieIcon,
+  },
+  {
+    name: "Security",
+    description: "lorem ipsum Security ",
+    href: "#",
+    icon: CursorArrowRaysIcon,
+  },
+  {
+    name: "Pharma",
+    description: "Your customers’ data will be safe and secure",
+    href: "#",
+    icon: FingerPrintIcon,
+  },
+  {
+    name: "Food",
+    description: "Connect with third-party tools",
+    href: "#",
+    icon: SquaresPlusIcon,
+  },
+  {
+    name: "Digital Passport",
+    description: "Build strategic funnels that will convert",
+    href: "#",
+    icon: ArrowPathIcon,
+  },
+];
+
 export default function Navbar() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="absolute max-w-7xl mx-auto inset-x-0 top-2 z-50">
+    <header className="absolute max-w-7xl mx-auto inset-x-0 top-2 z-10">
       <nav
-        className="flex items-center justify-between p-6 lg:px-8"
+        className="relative mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 z-50"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
@@ -29,6 +75,7 @@ export default function Navbar() {
             />
           </a>
         </div>
+
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -39,17 +86,75 @@ export default function Navbar() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="text-sm font-semibold leading-6 text-gray-900"
+
+        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+          <Popover className="relative">
+            <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900 outline-none">
+              Use cases
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              />
+            </Popover.Button>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
             >
-              {item.name}
-            </a>
-          ))}
-        </div>
+              <Popover.Panel className="absolute -left-8 top-full z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
+                <div className="p-4">
+                  {usecases.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
+                    >
+                      <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
+                        <item.icon
+                          className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="flex-auto">
+                        <a
+                          href={item.href}
+                          className="block font-semibold text-gray-900"
+                        >
+                          {item.name}
+                          <span className="absolute inset-0" />
+                        </a>
+                        <p className="mt-1 text-gray-600">{item.description}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </Popover.Panel>
+            </Transition>
+          </Popover>
+
+          <a
+            href="https://mangrovia.solutions"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            About us
+          </a>
+          <a
+            href="./faq"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Faq
+          </a>
+          <a
+            href="https://doc.datome.io/"
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            Docs
+          </a>
+        </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <a
             href="#"
@@ -65,20 +170,22 @@ export default function Navbar() {
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Datome</span>
-              <img
-                src="./datome.svg"
-                class="mr-01 h-10 fill-black"
-                alt="datome Logo"
-              />
-            </a>
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex justify-between">
+            <div className="flex lg:flex-1">
+              <a href="/" className="flex -m-1.5 p-1.5">
+                <span className="sr-only">datome</span>
+                <img
+                  src="./datome.svg"
+                  class="mr-01 h-10 fill-black"
+                  alt="datome Logo"
+                />
+              </a>
+            </div>
             <button
               type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              className="mr-0  text-gray-700"
               onClick={() => setMobileMenuOpen(false)}
             >
               <span className="sr-only">Close menu</span>
@@ -86,24 +193,46 @@ export default function Navbar() {
             </button>
           </div>
           <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-              <div className="py-6">
+            <div className="-my-6 ">
+              <div className=" py-6 divide-y-2 divide-gray-100">
+                <Disclosure as="div" className="-mx-3">
+                  {({ open }) => (
+                    <>
+                      <Disclosure.Button className="flex w-full items-center justify-between py-5 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-slate-100">
+                        Use cases
+                      </Disclosure.Button>
+                      <Disclosure.Panel className="mt-2 space-y-2">
+                        {usecases.map((item) => (
+                          <Disclosure.Button
+                            key={item.name}
+                            as="a"
+                            href={item.href}
+                            className="block py-1 pl-6 pr-3 text-sm leading-7 text-gray-600 hover:bg-gray-50"
+                          >
+                            {item.name}
+                          </Disclosure.Button>
+                        ))}
+                      </Disclosure.Panel>
+                    </>
+                  )}
+                </Disclosure>
                 <a
-                  href="#"
-                  className="rounded-md bg-black px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-primary-500  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+                  href="https://mangrovia.solutions"
+                  className="-mx-3 block px-3 py-5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
-                  Start trial <span aria-hidden="true">&rarr;</span>
+                  About us
+                </a>
+                <a
+                  href="./faq"
+                  className="-mx-3 block px-3 py-5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Faq
+                </a>
+                <a
+                  href="https://doc.datome.io/"
+                  className="-mx-3 block  px-3 py-5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Docs
                 </a>
               </div>
             </div>
