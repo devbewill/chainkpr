c9e6f2cb39815b5b550b90bc276fa0c1bdc6a142
devbewill <sedprc@gmail.com>
2023-09-27 12:49:54 +0200

    fixing

--- src/components/ProductInfo.js
+++ src/components/ProductInfo.js
@@ -2,7 +2,7 @@ export default function ProductInfo() {
   return (
     <section className="ProductInfo">
       <div className=" mx-auto max-w-7xl  ">
-        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+        <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl lg:flex lg:gap-x-16 lg:pl-10 lg:pt-0">
           <svg
             viewBox="0 0 1024 1024"
             className="absolute left-1/2 -z-10 h-[50rem] w-[50rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
@@ -22,11 +22,11 @@ export default function ProductInfo() {
               </radialGradient>
             </defs>
           </svg>
-          <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <div className="mx-auto max-w-lg text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+            <h2 className="text-3xl font-bold tracking-tight text-white md:text-5xl">
               Start using Datome today.
             </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-200">
+            <p className="mt-6 text-xl text-gray-100">
               Unsure how to solve your data management issues? <br></br>
               Boost your productivity and book a demo with us to see how Datome
               can help your organization.
