d82311f4650c7696b8856c1f6ae9757a3e86a1ea
devbewill <sedprc@gmail.com>
2023-09-28 22:47:26 +0200

    fixed post rendering

--- src/pages/article/[slug].js
+++ src/pages/article/[slug].js
@@ -27,37 +27,33 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  try {
-    const fileName = fs.readFileSync(`public/articles/${slug}.md`, "utf-8");
-    const { data: frontmatter, content } = matter(fileName);
+  const fileName = fs.readFileSync(`public/articles/${slug}.md`, "utf-8");
+  const { data: frontmatter, content } = matter(fileName);
 
-    return {
-      props: {
-        frontmatter,
-        content,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-
-    return {
-      props: {},
-    };
-  }
+  return {
+    props: {
+      frontmatter,
+      content,
+    },
+  };
 }
 
 function Article({ frontmatter, content }) {
-  // const tags = { frontmatter };
+  const { tags, title, socialImage } = frontmatter;
+
   return (
     <div className="prose p-5 mx-auto mt-8">
-      {/* {tags.map((tag) => (
-        <div>{tag}</div>
-      ))} */}
-      <h1>{frontmatter.title}</h1>
+      <h1>{title}</h1>
+
       <div className="relative w-full h-max-content overflow-hidden">
-        <img src={`/${frontmatter.socialImage}`} alt="" />
+        <img src={`/${socialImage}`} alt="" />
       </div>
       <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+      {tags.map((tag) => (
+        <div className="inline-flex rounded-md px-2 mr-2 bg-black text-white">
+          {tag}
+        </div>
+      ))}
     </div>
   );
 }
