248b5948dbf3d8c2597687aa6b5fd7a806824943
devbewill <sedprc@gmail.com>
2023-10-02 13:14:56 +0200

    readded widget link in hero section

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -3,7 +3,7 @@ export default function Announcement() {
     <div className="bg-primary px-4 py-1 text-white">
       <p className="text-center text-sm font-medium">
         Ready for Digital Passport?
-        <a href="#" className="inline-block underline px-1">
+        <a href="/article/usecase-dpp" className="inline-block underline px-1">
           Check the right way
         </a>
       </p>
